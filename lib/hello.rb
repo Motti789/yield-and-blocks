439c7f940b27529d18a75f71f357fdc8b6e0e133
Motti789 <mchillywilly@gmail.com>
2021-07-02 12:35:04 -0400

    commit tree fd492250834f0a931ef7c6cd8cb27ee9820ac9d8

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,19 @@
-def hello_t
+require "pry"
 
+def hello_t(array)
+if block_given?
+    i = 0
+
+    while i < array.length
+        yield array[i]
+      i = i + 1
+    end
+    array
+else 
+    puts "Hey! No block was given!"
+  end
 end
+#   binding.pry
 
 # call your method here!
 
